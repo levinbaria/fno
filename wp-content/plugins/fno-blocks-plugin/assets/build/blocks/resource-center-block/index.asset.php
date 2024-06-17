5cdca65f70fb99a4ddafc66daf4ee0ee85b3a231
Levin Baria <levin.baria@multidots.com>
2024-06-17 15:41:19 +0530

    Fixed the header menu fixed styling

--- wp-content/plugins/fno-blocks-plugin/assets/build/blocks/resource-center-block/index.asset.php
+++ wp-content/plugins/fno-blocks-plugin/assets/build/blocks/resource-center-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '9489103b853c64b8fef4');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '69314bc7176112accc19');
